70e87029dda51a14343ff1e2f2169fdbbeaa14eb
0805-18 <santiagoculma365@gmail.com>
2025-07-31 01:06:51 -0500

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     tabs.forEach(t => t.classList.remove('active'));
     contents.forEach(c => c.classList.remove('active'));
-    
     tab.classList.add('active');
     document.getElementById(tab.dataset.tab).classList.add('active');
   });
