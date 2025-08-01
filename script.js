60ea7d4b222782317f11be70673a6c585ccc3b33
0805-18 <santiagoculma365@gmail.com>
2025-08-01 01:09:05 -0500

    script.js

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   tabs.forEach(tab => {
     tab.addEventListener("click", () => {
-      // Remover la clase activa de todas las pestañas y contenidos
       tabs.forEach(t => t.classList.remove("active"));
       contents.forEach(c => c.classList.remove("active"));
 
-      // Activar la pestaña y contenido correspondiente
       tab.classList.add("active");
       const target = tab.getAttribute("data-tab");
       document.getElementById(target).classList.add("active");
